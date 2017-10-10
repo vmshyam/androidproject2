df15a00fbe63371bad3d8fb8b9dacb0f4bb03f90
Vivek Shyam <v.shyam@hotmail.com>
2017-10-10 15:11:14 -0400

    First Commit Login and Register layout work

---
+++ MobileApplicationPROG3210/app/src/main/java/com/example/ihp/mobileapplicationprog3210/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.ihp.mobileapplicationprog3210;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This Java file is related to 'activity_main.xml'
+ * The purpose of this file is to allow user to login
+ *      to the app. If user is not register within the
+ *      app, user must 'Sign Up' before logging in within
+ *      the app. User logs in with 'Username' and 'Password'
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameLogin;
+    private EditText passwordLogin;
+    private Button btnSignIn;
+    private TextView etSignUp;
+
+    private String usernameLoggedIn = null;
+    private String passwordLoggedIn = null;
+    private String regUsername = null;
+    private String regPassword = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameLogin = (EditText) findViewById(R.id.etUsernameMain);
+        passwordLogin = (EditText) findViewById(R.id.etPasswordMain);
+        btnSignIn = (Button) findViewById(R.id.btnSignInMain);
+        etSignUp = (TextView) findViewById(R.id.etSignUpMain);
+
+        regUsername = getIntent().getStringExtra("USERNAME_REG");
+        regPassword = getIntent().getStringExtra("PASSWORD_REG");
+
+        // When user clicks 'Sign In' button
+        btnSignIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                usernameLoggedIn = usernameLogin.getText().toString().trim();
+                passwordLoggedIn = passwordLogin.getText().toString().trim();
+
+                if (CheckLoginValidation()){
+                    Toast.makeText(LoginActivity.this,
+                            "Welcome to SnapSter", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    intent.putExtra("USERNAME", usernameLoggedIn);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(LoginActivity.this,
+                            "Please Enter Correct Username and Password", Toast.LENGTH_SHORT).show();
+                    usernameLogin.getText().clear();
+                    passwordLogin.getText().clear();
+                }
+            }
+        });
+
+        // When user clicks 'Sign Up' button
+        etSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /**
+     * This method checks if 'Sign In' 'Username' and 'Password' match registered 'Username' and
+     *      'Password'
+     * @return  'True' if 'Username' and 'Password' match with user registered information within
+     *              'Sign Up', else 'False'
+     */
+    public boolean CheckLoginValidation()
+    {
+        boolean checkResult = false;
+
+        if ((usernameLoggedIn.equals(regUsername)) && (passwordLoggedIn.equals(regPassword))){
+            checkResult = true;
+        }
+
+        return checkResult;
+    }
+}
---
+++ MobileApplicationPROG3210/app/src/main/java/com/example/ihp/mobileapplicationprog3210/RegisterActivity.java
@@ -0,0 +1,98 @@
+package com.example.ihp.mobileapplicationprog3210;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * This Java file is related to 'activity_register.xml'
+ * The purpose of this file is to allow user to Register
+ *      by providing personal and login information.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText accountNameValue;
+    private EditText usernameValue;
+    private EditText emailValue;
+    private EditText passwordValue;
+    private Button btnNewRegister;
+
+    private String usersName;
+    private String usersUsername;
+    private String userEmail;
+    private String userPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        accountNameValue = (EditText) findViewById(R.id.etNameReg);
+        usernameValue = (EditText) findViewById(R.id.etUsernameReg);
+        emailValue = (EditText) findViewById(R.id.etEmailReg);
+        passwordValue = (EditText) findViewById(R.id.etPasswordReg);
+        btnNewRegister = (Button) findViewById(R.id.btnSignUpReg);
+
+        // When user click 'Sign Up' button
+        btnNewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!ValidateRegistrationInputs()){
+                    Toast.makeText(RegisterActivity.this,
+                            "Registration was Unsuccessful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this,
+                            "Successfully Registered", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    intent.putExtra("USERNAME_REG", usersUsername);
+                    intent.putExtra("PASSWORD_REG", userPassword);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    /**
+     * This method checks all the text fields for valid input values before allowing the user
+     *      to register.
+     * @return  Returns 'true' if all text fields have acceptable values, else 'false'
+     */
+    public boolean ValidateRegistrationInputs(){
+        boolean inputValueCheck = true;
+        usersName = accountNameValue.getText().toString().trim();
+        usersUsername = usernameValue.getText().toString().trim();
+        userEmail = emailValue.getText().toString().trim();
+        userPassword =  passwordValue.getText().toString().trim();
+
+        if (usersName.isEmpty()){
+            accountNameValue.setError("Please Enter a Valid Name");
+            accountNameValue.getText().clear();
+            inputValueCheck = false;
+        }
+
+        if (usersUsername.isEmpty()){
+            usernameValue.setError("Please Enter a Username");
+            usernameValue.getText().clear();
+            inputValueCheck = false;
+        }
+
+        if (userEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+            emailValue.setError("Please Enter a Valid email");
+            emailValue.getText().clear();
+            inputValueCheck = false;
+        }
+
+        if (userPassword.isEmpty()){
+            passwordValue.setError("Please Enter a Valid Password");
+            passwordValue.getText().clear();
+            inputValueCheck = false;
+        }
+
+        return inputValueCheck;
+    }
+}
